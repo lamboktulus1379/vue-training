11e5425aced4d5392f64005f7bf1dc3780625d2f
lamboktulus1379 <lamboktulus1379@gmail.com>
2019-05-23 17:07:57 +0700

    Added tooltip component

---
+++ src/components/boxTooltip.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="tooltip-container">
+    <div class="tooltip">{{ tooltip }}</div>
+    <slot/>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => {
+    return {};
+  },
+  props: ["tooltip"]
+};
+</script>
+
+<style lang="scss" scoped>
+.tooltip-container {
+  .tooltip {
+    display: none;
+  }
+  &:hover {
+    .tooltip {
+      display: block;
+      position: absolute;
+      top: -30px;
+      width: 100px;
+      left: 0;
+      background: #f5f5f5;
+      color: black;
+      border-radius: 15px;
+      padding: 2.5px;
+      font-size: 0.75em;
+      text-align: center;
+      z-index: 1111;
+    }
+  }
+}
+</style>
