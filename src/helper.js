3cde29a6d4006630e2c0a17d00fea40246908ba6
lamboktulus1379 <lamboktulus1379@gmail.com>
2019-05-25 16:11:28 +0700

    Add time function

--- src/helper.js
+++ src/helper.js
@@ -146,6 +146,23 @@ export default Vue.mixin({
       ctx.moveTo(75, 0);
       ctx.lineTo(200, 100);
       ctx.stroke();
+    },
+
+    convertMilliseconds(milliseconds) {
+      var day, hour, minute, seconds;
+      seconds = Math.floor(milliseconds / 1000);
+      minute = Math.floor(seconds / 60);
+      seconds = seconds % 60;
+      hour = Math.floor(minute / 60);
+      minute = minute % 60;
+      day = Math.floor(hour / 24);
+      hour = hour % 24;
+      return {
+        day: day,
+        hour: hour,
+        minute: minute,
+        seconds: seconds
+      };
     }
   }
 });
